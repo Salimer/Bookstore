c39cea35e0ec6fe4b50cffb3ffc573a73037b713
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-08 14:45:21 +0300

    Update ListBooks.js

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Book from './Book';
 import AddBook from './AddBook';
 
 const ListBooks = () => {
-  const books = [
+  const [books, setBooks] = useState([
     {
       id: 1,
       title: 'The Hunger Games',
@@ -12,13 +13,29 @@ const ListBooks = () => {
       completed: '64%',
       chapter: 'Chapter 17',
     },
-  ];
+  ]);
+
+  const handleAddBookClick = (e) => {
+    e.preventDefault();
+    console.log('The link was clicked.');
+    const newBook = {
+      id: 2,
+      title: 'Holy Moly',
+      author: 'Salim',
+      category: 'Action',
+      genre: 'Action',
+      completed: '40%',
+      chapter: '13',
+    };
+    setBooks((prev) => [...prev, newBook]);
+    console.log(books);
+  };
   return (
     <section>
-        {books.map((book) => (
-            <Book key={book.id} book={book} />
-            ))}
-        <AddBook />
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+      <AddBook handleAddBookClick={handleAddBookClick} />
     </section>
   );
 };
