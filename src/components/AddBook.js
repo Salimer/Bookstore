137b8933da5879bcbb7fb1b54d116f32206788b5
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-08 17:46:21 +0300

    Fix percentage circle

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -32,8 +32,10 @@ AddBook.propTypes = {
 export default AddBook;
 
 const Section = styled.section`
+border-top: 1px solid #e8e8e8;
+margin: 2rem 6.25rem;
     .title {
-        width: 10.875rem;
+        // : 10.875rem;
         height: 1.5rem;
         margin: 1.813rem 32.75rem 1.188rem 0;
         font-size: 1.25rem;
@@ -46,33 +48,30 @@ const Section = styled.section`
     }
     .form {
         display: flex;
+        justify-content: space-between;
 
         .book-title {
-            width: 41.5rem;
             height: 2.813rem;
-            margin: 1.188rem 2.125rem 0 0;
-            padding: 0.813rem 35.688rem 0.813rem 1.063rem;
             border-radius: 4px;
             border: solid 1px var(--white);
             background-color: #fff;
+            padding-left: 1rem;
+            width: 55.15%;
         }
         .book-author {
-            width: 17.813rem;
             height: 2.813rem;
-            margin: 1.188rem 2.125rem 0;
-            padding: 0.375rem 0.938rem 0.563rem 1rem;
             border-radius: 4px;
             border: solid 1px var(--white);
             background-color: #fff;
+            padding-left: 1rem;
+            width: 23.67%;
         }
         .add-book {
-            width: 11.5rem;
             height: 2.813rem;
-            margin: 1.188rem 0 0 2.125rem;
-            padding: 0.801rem 1.188rem 0.886rem 1.375rem;
             border-radius: 3px;
             color: #fff;
             background-color: #2e90f0;
+            width: 15.28%;
         }
     }
 
