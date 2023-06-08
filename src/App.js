286ebf1bfb971c3dbb94e1a4271a0f031448654e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-08 16:49:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Categories from './routes/Categories';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
