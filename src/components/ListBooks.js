e1305d36510f35cd2b255caa96b97275ea7f62a9
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-08 15:26:42 +0300

    Update ListBooks.js

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 import AddBook from './AddBook';
 
 const ListBooks = () => {
-  const [books, setBooks] = useState([
-    {
-      id: 1,
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      category: 'Action',
-      genre: 'Action',
-      completed: '64%',
-      chapter: 'Chapter 17',
-    },
-  ]);
+  const [books, setBooks] = useState(() => {
+    const storedBooks = localStorage.getItem('booksData');
+    return storedBooks ? JSON.parse(storedBooks) : [];
+  });
 
+  const saveBooksToLocalStorage = (items) => {
+    localStorage.setItem('booksData', JSON.stringify(items));
+  };
+
+  useEffect(() => {
+    saveBooksToLocalStorage(books);
+  }, [books]);
   const handleAddBookClick = (title, author) => {
     const newBook = {
       id: uuidv4(),
@@ -29,10 +29,14 @@ const ListBooks = () => {
     setBooks((prev) => [...prev, newBook]);
     console.log(books);
   };
+
+  const handleRemoveBookClick = (id) => {
+    setBooks((prev) => prev.filter((book) => book.id !== id));
+  };
   return (
     <section>
       {books.map((book) => (
-        <Book key={book.id} book={book} />
+        <Book key={book.id} book={book} handleRemoveBookClick={handleRemoveBookClick} />
       ))}
       <AddBook handleAddBookClick={handleAddBookClick} />
     </section>
