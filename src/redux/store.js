98b4f5d2222045aff1986c1ade58ae2635677349
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-12 14:56:52 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksArrSlice from './books/booksArrSlice';
+import categoriesSlice from './categories/categoriesSlice'
 
 const store = configureStore({
   reducer: {
     bookstore: booksArrSlice,
+    categories: categoriesSlice,
   },
 });
 
