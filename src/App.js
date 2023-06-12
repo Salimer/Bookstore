1b20a2133a698585193f51d1df38e258fbd0917b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-12 15:53:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Home from './routes/Home';
 import Categories from './routes/Categories';
 import './App.css';
 import Navbar from './components/Navbar';
+import store from './redux/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
-    </>
+    </Provider>
   );
 }
 
