6867e3f6ed8f41129923f8db7563818286bf0d96
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-12 14:33:06 +0300

    Update categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,5 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    categories: [];
-}
+  categories: [],
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    setStatus: (state) => {
+      state.categories = 'Under construction';
+    },
+  },
+});
+
+export const { setStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
