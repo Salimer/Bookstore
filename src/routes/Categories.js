ec43fb826c3e1792f37d0722a92703e09f755529
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-13 16:55:19 +0300

    Update Categories.js

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -1,2 +1,20 @@
-const Categories = () => <div>Categories</div>;
+import styled from 'styled-components';
+
+const Categories = () => (
+  <Div>
+    <h1>Under Construction</h1>
+  </Div>
+);
+
 export default Categories;
+
+const Div = styled.section`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+
+  h1 {
+    font-size: 3rem;
+    text-align: center;
+  }
+`;
