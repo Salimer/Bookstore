37dd70de938528858ffd4863e26067acd9934ecb
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-13 13:52:56 +0300

    Update ListBooks.js

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,24 +1,44 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
+import { getBooks } from '../redux/books/booksArrSlice';
+
+const Loading = () => (
+  <div className="loading">
+    <h1>Loading...</h1>
+  </div>
+);
 
 const ListBooks = () => {
-  const books = useSelector((store) => store.bookstore.books);
+  const dispatch = useDispatch();
+  const { books, isLoading } = useSelector((store) => store.books);
 
   const saveBooksToLocalStorage = (items) => {
     localStorage.setItem('booksData', JSON.stringify(items));
   };
 
   useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  useEffect(() => {
     saveBooksToLocalStorage(books);
   }, [books]);
+
+  console.log(books);
   return (
     <section>
-      {books.map((book) => (
-        <Book key={book.item_id} book={book} />
-      ))}
-      <AddBook />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {books.map((book) => (
+            <Book key={book.item_id} book={book} />
+          ))}
+          <AddBook />
+        </>
+      )}
     </section>
   );
 };
